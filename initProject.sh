44f96accd4a4629f98a61c549d6022fabebef5f1
lantos1618 <l.leong1618@gmail.com>
2023-06-15 07:47:53 +0200

    init file structure

--- initProject.sh
+++ initProject.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create directories
-mkdir -p distributed-task-scheduler/{task-definition-service,task-scheduler-service,task-execution-service,database-service,user-interface}/{handlers}
+mkdir -p distributed-task-scheduler/{task-definition-service,task-scheduler-service,task-execution-service,database-service,cli-user-interface}/{handlers}
 mkdir -p distributed-task-scheduler/proto
 
 # Create main.go and Dockerfile in each service directory
@@ -19,7 +19,7 @@ touch distributed-task-scheduler/docker-compose.yml
 
 # Initialize Go modules
 cd distributed-task-scheduler
-for service in task-definition-service task-scheduler-service task-execution-service database-service user-interface
+for service in task-definition-service task-scheduler-service task-execution-service database-service cli-user-interface
 do
     cd ${service}
     go mod init zug.dev/${service}
